409fd2dfe848b4593b8954ecd89728e69eb4e104
yoojinc3 <yoojinc3@illinois.edu>
2025-08-13 20:28:18 -0500

    add mac-sql

---
+++ MAC-SQL/core/api_config.py
@@ -0,0 +1,6 @@
+MODEL_NAME = 'gpt-4o'
+# MODEL_NAME = 'gpt-4-1106-preview' # 128k 版本
+# MODEL_NAME = 'CodeLlama-7b-hf'
+# MODEL_NAME = 'gpt-4-32k' # 0613版本
+# MODEL_NAME = 'gpt-4' # 0613版本
+# MODEL_NAME = 'gpt-35-turbo-16k' # 0613版本
